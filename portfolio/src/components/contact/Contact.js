b886775ea51e00a0ed5ea9963e2d0671e1dc93ae
Suparada Saitalae <ann.suparada@gmail.com>
2019-11-20 10:01:46 -0600

    making contact

--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon, Row, Col } from 'antd';
 import './contact.scss'
 
 
@@ -7,11 +8,22 @@ const Contact = () => {
 
     return (
         <div className="contact-container">
-            <div classname="contact-card">
-                <h1>Contact</h1>
+            <div className="contact-card">
+                    <div className="contact-left">
+                        <h1>Contact</h1>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    </p>
+                    </div>
+                    <div className="contact-right">
+                        <h2>Contact Info</h2>
+                        <Icon type="github" />
+                        <Icon type="linkedin" />
+                        <Icon type="mail" />
+                    </div>
             </div>
         </div>
     )
-} 
+}
 
 export default Contact;
