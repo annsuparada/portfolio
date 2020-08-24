3f8e3c44bdb27d73fe13e489aa0d4c6fb3b109a6
Ann <ann.suparada@gmail.com>
2020-08-24 13:49:11 -0500

    updated skills

--- portfolio/src/components/portfolio/Portfolio.js
+++ portfolio/src/components/portfolio/Portfolio.js
@@ -1,33 +1,40 @@
 import React from 'react';
-import WarStories from './WarStories'
-import NodeJs from './NodeJs';
-import MazeGame from './MazeGame'
 import RecipeFinder from './RecipeFinder';
 import TheObservatory from './TheObservatory';
 import './portfolio.scss'
-
+import { Tag } from 'antd';
 
 
 const Portfolio = () => {
 
     return (
         <>
-        <div className="protfolio-container" id="portfolio">
+
+            <div className="protfolio-container" id="portfolio">
+                    <h1>Skills</h1>
+                <div className="skills">
+                    <div>HTML5</div>
+                    <div>CSS</div>
+                    <div>Javascript</div>
+                    <div>React</div>
+                    <div>Redux</div>
+                    <div>Node JS</div>
+                    <div>Express</div>
+                    <div>Postgres</div>
+                    <div>Python</div>
+                    <div>Django</div>
+                    <div>Flask</div>
+                    <div>Shopify</div>
+                </div>
                 <h1 >Portfolio</h1>
-            <div className="card-wrapper">
-                <RecipeFinder />
-                <TheObservatory />
+                <div className="card-wrapper">
+                    <RecipeFinder />
+                    <TheObservatory />
+                </div>
+
             </div>
-            <div className="card-wrapper">
-                <NodeJs />
-                <MazeGame />
-            </div>
-            <div className="card-wrapper">
-                <WarStories />
-            </div>
-        </div>
-       </>
+        </>
     )
-} 
+}
 
 export default Portfolio;
